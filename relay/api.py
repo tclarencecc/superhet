7b10b135b854892d720b3cce4051ce1a867398b5
Clarence <@>
2024-11-19 19:23:43 +0800

    agent html

--- relay/api.py
+++ relay/api.py
@@ -11,11 +11,12 @@ def _has_agent(req: Request, agent: str) -> Response | None:
         return Response("Agent unavailable", status_code=404)
 
 
-@route("GET", "/a/{agent:str}/query", middlewares=[_has_agent])
+@route("POST", "/a/{agent:str}/query", middlewares=[_has_agent])
 async def query(req: Request, agent: str):
+    form = await req.json()
     query = Query()
     query.id = uuid.uuid4().hex
-    query.text = "what is crit rate"
+    query.text = form["query"]
 
     ws = Agents.websocket(agent)
     stg = Agents.stream(agent, query.id).new()
--- relay/decorator.py
+++ relay/decorator.py
@@ -1,7 +1,10 @@
 import functools
+import traceback
 from starlette.requests import Request
+from starlette.responses import Response
 from starlette.routing import Route
 from typing import Callable
+from common.helper import PrintColor
 
 def route(method: str, path: str, middlewares: list[Callable[[Request, any], any]]=None):
     def decorate(fn):
@@ -15,9 +18,13 @@ def route(method: str, path: str, middlewares: list[Callable[[Request, any], any
                         ret = mw(req, *params)
                         if ret is not None:
                             return ret
-
-                ret = await fn(req, *params)
-                return ret
+                
+                try:
+                    ret = await fn(req, *params)
+                    return ret
+                except Exception:
+                    PrintColor.ERROR(traceback.format_exc())
+                    return Response(status_code=500)
 
             return Route(path, endpoint, methods=[method])
         return wrapper
