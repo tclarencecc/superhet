5e38f831a0f88141612d6c56f94ae5109ed235a1
Clarence <@>
2024-12-12 16:29:54 +0800

    refactored llm_ex

--- agent/llm_ex.py
+++ agent/llm_ex.py
@@ -1,9 +1,10 @@
 from llama_cpp import Llama, llama_chat_format
-from typing import Iterable
+from typing import Iterable, Self
 import time
 import json
 
 from common.decorator import suppress_print
+from agent.llm import Chat
 
 class _Llama:
     class _Config:
@@ -46,55 +47,126 @@ class _Llama:
         # on Llama constructor, verbose is initialized based on cfg.debug
         # to disable runtime verbosity, set false after constructor call i.e. llama_perf_context_print
         self._llama.verbose = False
+
+        self._comp_text: str = None
+        self._comp_chat: list = None
+        self._grammar = None
+        self._benchmark = False
+
+    # grammar = {
+    #     "type": "array",
+    #     "items": {
+    #         "type": "object",
+    #         "properties": {
+    #             "question": { "type": "string" },
+    #             "answer": { "type": "string" }
+    #         },
+    #         "required": ["question", "answer"]
+    #     },
+    #     "minItems": 3,
+    #     "maxItems": 100
+    # }
+    def __call__(self, input: str | dict, grammar: dict=None, benchmark=False) -> Self:
+        self._comp_text = None
+        self._comp_chat = None
+        self._grammar = grammar
+        self._benchmark = benchmark
+
+        if type(input) is str:
+            self._comp_text = input
+
+        elif type(input) is dict:
+            msg = []
+            if "system" in input:
+                msg.append({
+                    "role": "system",
+                    "content": input["system"]
+                })
+            if "chat" in input:
+                chat: Chat = input["chat"]
+                for v in chat._deque:
+                    if v is not None:
+                        msg.append({
+                            "role": "user",
+                            "content": v.query
+                        })
+                        msg.append({
+                            "role": "assistant",
+                            "content": v.answer
+                        })
+            if "user" in input:
+                msg.append({
+                    "role": "user",
+                    "content": input["user"]
+                })
+
+            self._comp_chat = msg
         
-    def stream(self, prompt: str, timed=True) -> Iterable[str]:
-        res = self._llama.create_completion(prompt,
-            max_tokens=None,
-            stream=True,
-            temperature=self._config.temperature
-        )
-        count = 0
-        t = time.time()
-
-        for r in res:
-            count += 1
-            yield r["choices"][0]["text"]
-
-        if timed:
-            t = time.time() - t
-            yield f"\n{t:.1f} sec @ {(count / t):.1f} token/sec"
-
-    def static(self, prompt: str) -> str:
-        res = self._llama.create_completion(prompt,
-            max_tokens=None,
-            temperature=self._config.temperature
-        )
-        return res["choices"][0]["text"]
+        return self
     
-    def json(self, prompt: str, schema: dict) -> any:
-        # schema = {
-        #     "type": "array",
-        #     "items": {
-        #         "type": "object",
-        #         "properties": {
-        #             "question": { "type": "string" },
-        #             "answer": { "type": "string" }
-        #         },
-        #         "required": ["question", "answer"]
-        #     },
-        #     "minItems": 3,
-        #     "maxItems": 100
-        # }
-        grammar = llama_chat_format._grammar_for_response_format({
-            "type": "json_object",
-            "schema": schema
-        })
-        res = self._llama.create_completion(prompt,
+    def _completion(self, stream: bool) -> any:
+        grammar = None
+        if self._grammar is not None:
+            grammar = llama_chat_format._grammar_for_response_format({
+                "type": "json_object",
+                "schema": self._grammar
+            })
+
+        return self._llama.create_completion(self._comp_text,
             max_tokens=None,
+            stream=stream,
             temperature=self._config.temperature,
             grammar=grammar
         )
-        return json.loads(res["choices"][0]["text"])
+    
+    def _chat_completion(self, stream: bool) -> any:
+        response_format = None
+        if self._grammar is not None:
+            response_format = {
+                "type": "json_object",
+                "schema": self._grammar
+            }
+
+        return self._llama.create_chat_completion(self._comp_chat,
+            max_tokens=None,
+            stream=stream,
+            temperature=self._config.temperature,
+            response_format=response_format
+        )
+
+    @property
+    def stream(self) -> Iterable[str]:
+        count = 0
+        t = time.time()
+
+        if self._comp_text is not None:
+            for r in self._completion(True):
+                count += 1
+                yield r["choices"][0]["text"]
+                
+        elif self._comp_chat is not None:
+            for r in self._chat_completion(True):
+                count += 1
+                if "content" in r["choices"][0]["delta"]:
+                    yield r["choices"][0]["delta"]["content"]
+
+        if self._benchmark:
+            t = time.time() - t
+            yield f"\n{t:.1f} sec @ {(count / t):.1f} token/sec"
+
+    @property
+    def static(self) -> str:
+        if self._comp_text is not None:
+            res = self._completion(False)
+            return res["choices"][0]["text"]
+        
+        elif self._comp_chat is not None:
+            res = self._chat_completion(False)
+            return res["choices"][0]["message"]["content"]
+
+    @property
+    def json(self) -> any:
+        return json.loads(self.static)
 
 
 class _LlmMeta(type):
